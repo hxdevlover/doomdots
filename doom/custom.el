a5577f3fa288d7b4569474fac10936c3efa07aea
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-06-25 09:03:24 +0430

    Update custom.el

--- doom/custom.el
+++ doom/custom.el
@@ -13,20 +13,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- 
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- 
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
 
 ;; lsp config
 (use-package lsp
